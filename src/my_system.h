71f5cac7dd3d9ac5113e863aab49b5e5ebfb7a11
lilydjwg <lilydjwg@gmail.com>
2010-12-13 22:11:55 +0800

    卸载 vjde
    1. 启动速度变慢（~0.04s）
    2. 在不需要它的时候加载

--- src/my_system.h
+++
@@ -1,4 +0,0 @@
-#ifndef MY_SYSTEM_H
-#define MY_SYSTEM_H
-unsigned int my_system(const char *cmd,char *result,int maxLen);
-#endif
--- src/previewwindow.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef _PREVIEWWINDOW_HPP
-#define _PREVIEWWINDOW_HPP
-char* preview_window(vector<const string> &v,const string& base,int x,int y,int width,int height);
-#endif
-//test
-
