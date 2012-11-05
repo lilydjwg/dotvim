a70704a7b210a299df52f47dab8aeb9d1726c5ea
依云 <lilydjwg@gmail.com>
2012-11-05 13:04:48 +0800

    pasting supports gzipped data

--- vimrc.py
+++ vimrc.py
@@ -9,7 +9,7 @@ def LilyPaste():
   msg = 'Pasting...'
   echon(msg)
   vim.command('redraw')
-  curl = subprocess.Popen(['curl', '-m', '60', '-Ss', '-F', 'vimcn=<-', 'http://p.vim-cn.com'],
+  curl = subprocess.Popen(['curl', '--compressed', '-m', '60', '-Ss', '-F', 'vimcn=<-', 'http://p.vim-cn.com'],
                           stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                          )
   for l in vim.current.range:
