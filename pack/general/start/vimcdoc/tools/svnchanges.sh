3c218f68514f9a1fc936ccdcaef28a470a3abf3b
依云 <lilydjwg@gmail.com>
2017-08-18 18:02:37 +0800

    git subrepo clone git@github.com:yianwillis/vimcdoc.git pack/general/start/vimcdoc
    subrepo:
      subdir:   "pack/general/start/vimcdoc"
      merged:   "564e8709"
    upstream:
      origin:   "git@github.com:yianwillis/vimcdoc.git"
      branch:   "master"
      commit:   "564e8709"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ pack/general/start/vimcdoc/tools/svnchanges.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+version=VIMCDOC_`cat ../VERSION | sed -e 's/\./_/g'`
+last_rev=`svn info ../../tags/$version | grep 'Last Changed Rev:' | cut -d ' ' -f 4`
+
+# decect added new files since last tagged release
+list=/tmp/vimcdoc-list.$$
+added=/tmp/vimcdoc-added.$$
+cd ../doc
+ls > $list
+ls ../../tags/$version/doc | comm -3 - $list > $added
+if [ -s $added ]; then
+	echo '>>>> NEW FILES SINCE LAST RELEASE <<<<'
+	cat $added
+	echo -e '\n'
+fi
+rm $list $added
+
+# output updating info of various files
+change=/tmp/vimcdoc-change.$$
+echo '>>>> UPDATED FILES SINCE LAST RELEASE <<<'
+for i in *.txt
+do
+    svn log -r $last_rev:HEAD  $i > $change
+    len=`cat $change | wc -l`
+    if [ $len -gt 1 ]; then
+	    echo -e "~~~~\t$i\t~~~~"
+	    cat $change | sed -e '/^-----/d' -e '/^r[0-9]\+/d' -e '/^\s*$/d'
+	    echo -e '\n'
+    fi
+done
+rm $change
