59876bc2e1dc4b4f0ac73e724c8bcb70a88f47b6
依云 <lilydjwg@gmail.com>
2020-12-21 19:47:18 +0800

    update fcitx.vim to fcitx5 version

--- plugin/fcitx.py
+++ plugin/fcitx.py
@@ -1,70 +1,23 @@
-# vim:fileencoding=utf-8
-
-import os
 import vim
-import socket
-import struct
-import contextlib
 
-fcitxsocketfile = vim.eval('s:fcitxsocketfile')
+import dbus
 
-class FcitxComm(object):
-  STATUS     = struct.pack('i', 0)
-  ACTIVATE   = struct.pack('i', 1 | (1 << 16))
-  DEACTIVATE = struct.pack('i', 1)
-  INT_SIZE   = struct.calcsize('i')
-
-  def __init__(self, socketfile):
-    if socketfile[0] == '@': # abstract socket
-      socketfile = '\x00' + socketfile[1:]
-    self.socketfile = socketfile
-    self.sock = None
+class FcitxComm():
+  def __init__(self):
+    bus = dbus.SessionBus()
+    obj = bus.get_object('org.fcitx.Fcitx5', '/controller')
+    self.fcitx = dbus.Interface(obj, dbus_interface='org.fcitx.Fcitx.Controller1')
 
   def status(self):
-    return self._with_socket(self._status) == 2
+    return self.fcitx.State() == 2
 
   def activate(self):
-    self._with_socket(self._command, self.ACTIVATE)
+    self.fcitx.Activate()
 
   def deactivate(self):
-    self._with_socket(self._command, self.DEACTIVATE)
+    self.fcitx.Deactivate()
 
-  def _error(self, e):
-    estr = str(e).replace('"', r'\"')
-    file = self.socketfile.replace('"', r'\"')
-    if file and file[0] == '\0':
-      file = '@' + file[1:]
-    vim.command('echohl WarningMsg | echo "fcitx.vim: socket %s error: %s" | echohl NONE' % (file, estr))
-
-  def _connect(self):
-    self.sock = sock = socket.socket(socket.AF_UNIX)
-    sock.settimeout(0.5)
-    try:
-      sock.connect(self.socketfile)
-      return True
-    except (socket.error, socket.timeout) as e:
-      self._error(e)
-      return False
-
-  def _with_socket(self, func, *args, **kwargs):
-    # fcitx doesn't support connection reuse
-    if not self._connect():
-      return
-
-    with contextlib.closing(self.sock):
-      try:
-        return func(*args, **kwargs)
-      except (socket.error, socket.timeout, struct.error) as e:
-        self._error(e)
-
-  def _status(self):
-    self.sock.send(self.STATUS)
-    return struct.unpack('i', self.sock.recv(self.INT_SIZE))[0]
-
-  def _command(self, cmd):
-    self.sock.send(cmd)
-
-Fcitx = FcitxComm(fcitxsocketfile)
+Fcitx = FcitxComm()
 
 def fcitx2en():
   if Fcitx.status():
